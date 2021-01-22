2f246a406397e2e7c716cbb70d47d36b75b533fe
Barbara Jeziorek <barbarajeziorek@gmail.com>
2021-01-22 17:26:01 +0100

    parser and source

---
+++ kanjiExtractor.py
@@ -0,0 +1,77 @@
+import re
+f=open("kanji_tbody.html", encoding="utf8")
+
+f.seek(0,2)
+end=f.tell()
+f.seek(0)
+
+keyNames=['nr','New','Old','Radical','Strokes','Grade','Year_added','English_meaning','Readings']
+iterator=0
+x="jouyouKanji=["
+
+iterator=1
+
+while(f.tell()<end):
+ x=x+u"\n\t{\n"
+
+ for i in range(0,11):
+  if i==0:
+   m=f.readline()  
+  
+ 
+  if i<10:
+   m=f.readline()
+   
+   if i!=9:
+    g=str.rstrip(m)
+    g=re.sub('\</td>','',g)
+    g=re.sub('\</tr>','',g)
+    g=re.sub('<td.*?>','',g)
+    g=re.sub('<a.*?>','',g)
+    g=re.sub('\<tr>','',g)
+    g=re.sub('\</a>','',g)
+    g=str.rstrip(g)
+   
+    x=x+"\t\t"+keyNames[i]+":'"+g+"',\n"
+   else:
+    x=re.sub(',\s$','',x)
+	
+ x=x+"\n\t}"
+ if f.tell()!=end:
+  x=x+","
+x=x+"\n];"
+
+m=open("results.txt","w",encoding="utf8")
+m.write(x)
+
+#-----------------------------------------------------------
+
+# #1
+# m=f.readline()
+# x=x+m+"',x2:"
+
+# #2
+# m=f.readline()
+# x=x+m+"',x3:"
+
+# #3
+# m=f.readline()
+# x=x+m+"',x4:"
+
+# #4
+# m=f.readline()
+# x=x+m+"',x5:"
+
+# #5
+# m=f.readline()
+# x=x+m+"',x6:"
+
+
+
+
+#print (x)
+
+
+#wytnik od strungu x do strungu y bez nich
+
+
